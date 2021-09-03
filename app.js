f682843c3a77b4e775a04368dd6344512e7933a4
Andrew <kentuxa@icloud.com>
2021-09-03 14:50:17 +0300

    new settings for the slick slider

--- app.js
+++ app.js
@@ -75,30 +75,127 @@ modal.addEventListener("click", function (e) {
 //   }
 // };
 
-let scroller = document.addEventListener(
-  "scroll",
-  function () {
-    let scrolling = document.scrollY !== 0;
-    scrolling = Math.round(scrollY);
+// let scroller = document.addEventListener(
+//   "scroll",
+//   function () {
+//     let scrolling = document.scrollY !== 0;
+//     scrolling = Math.round(scrollY);
 
-    if (scrolling >= fifthPercent) {
-      showModal();
-    } else {
-      closeModal();
-    }
+//     if (scrolling >= fifthPercent) {
+//       showModal();
+//     } else {
+//       closeModal();
+//     }
+//   }
+//   // { once: true }
+// );
+
+// let scrollHeight = Math.max(
+//   document.body.scrollHeight,
+//   document.documentElement.scrollHeight,
+//   document.body.offsetHeight,
+//   document.documentElement.offsetHeight,
+//   document.body.clientHeight,
+//   document.documentElement.clientHeight
+// );
+
+// let b = document.documentElement.clientHeight;
+
+// let fifthPercent = scrollHeight / 2;
+
+function showModalByScroll() {
+  if (window.pageYOffset >= document.body.scrollHeight / 2) {
+    showModal();
+    window.removeEventListener("scroll", showModalByScroll);
   }
-  // { once: true }
+}
+
+window.addEventListener("scroll", showModalByScroll);
+
+// change product quantity
+
+let decrementBtn = document.querySelectorAll(".decrement-button");
+let incrementBnt = document.querySelectorAll(".increment-button");
+let quantityInput = document.querySelectorAll(".product-quantity input");
+let minCount = 1;
+let maxCount = 5;
+
+function toggleBtnState(count, decrementBtn, incrementBnt) {
+  decrementBtn.disabled = count <= minCount;
+  incrementBnt.disabled = count >= maxCount;
+}
+
+quantityInput.forEach((item, i) =>
+  toggleBtnState(item.value, decrementBtn[i], incrementBnt[i])
+);
+decrementBtn.forEach((item, i) =>
+  item.addEventListener("click", function () {
+    let currentValue = +quantityInput[i].value;
+    let nextValue = currentValue - 1;
+    quantityInput[i].value = nextValue;
+    toggleBtnState(nextValue, item, incrementBnt[i]);
+  })
 );
 
-let scrollHeight = Math.max(
-  document.body.scrollHeight,
-  document.documentElement.scrollHeight,
-  document.body.offsetHeight,
-  document.documentElement.offsetHeight,
-  document.body.clientHeight,
-  document.documentElement.clientHeight
+incrementBnt.forEach((item, i) =>
+  item.addEventListener("click", function () {
+    let currentValue = +quantityInput[i].value;
+    let nextValue = currentValue + 1;
+    quantityInput[i].value = nextValue;
+    toggleBtnState(nextValue, decrementBtn[i], item);
+  })
 );
 
-let b = document.documentElement.clientHeight;
+// let currentValue = +quantityInput;
+// toggleBtnState(currentValue);
 
-let fifthPercent = scrollHeight / 2;
+// slider slick
+
+$(".slider-block").slick({
+  dots: true,
+});
+
+// // Counter function constructor
+// let decrementBtns = document.querySelectorAll(".decrement-button")[0];
+// let incrementBtns = document.querySelectorAll(".increment-button")[0];
+// let quantityInput = document.querySelectorAll(".product-quantity input")[0];
+
+// function Car(year, model, color) {
+//   this.year = year;
+//   this.model = model;
+//   this.color = color;
+// }
+
+// let honda = new Car("2008", "civic", "gold");
+// let audi = new Car("2016", "a4", "black");
+// console.log(honda);
+// console.log(audi);
+
+// function Calculator(a, b) {
+//   this.a = a;
+//   this.b = b;
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mult = function () {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calc = new Calculator(10, 5);
+// console.log(calc.sum());
+
+// function Counter(incrementBnt, decrementBtn, inputField) {
+// this.incrementBnt = incrementBnt;
+// this.decrementBtn = decrementBtn;
+// this.inputField = inputField;
+//   this.domRefs = {
+//     incrementBnt,
+//     decrementBtn,
+//     inputField,
+//   };
+// }
+
+// const counter = new Counter(incrementBnt, decrementBtn, inputField);
+// console.log(counter);
